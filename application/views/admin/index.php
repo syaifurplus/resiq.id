6f48a1ed7db0c3e0254b525194d13b84c68354d4
ekkitaufikm <79741859+ekkitaufikm@users.noreply.github.com>
2021-08-24 12:22:42 +0700

    update

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,3 +1,6 @@
+<?php
+include('layout/header.php');
+?>    
     <section class="content">
         <div class="container-fluid">
             <div class="block-header">
@@ -52,36 +55,6 @@
                 </div>
             </div>
             <!-- #END# Widgets -->
-            <!-- CPU Usage -->
-            <div class="row clearfix">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <div class="card">
-                        <div class="header">
-                            <div class="row clearfix">
-                                <div class="col-xs-12 col-sm-6">
-                                    <h2>CPU USAGE (%)</h2>
-                                </div>
-                            </div>
-                            <ul class="header-dropdown m-r--5">
-                                <li class="dropdown">
-                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                        <i class="material-icons">more_vert</i>
-                                    </a>
-                                    <ul class="dropdown-menu pull-right">
-                                        <li><a href="javascript:void(0);">Action</a></li>
-                                        <li><a href="javascript:void(0);">Another action</a></li>
-                                        <li><a href="javascript:void(0);">Something else here</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="body">
-                            <div id="real_time_chart" class="dashboard-flot-chart"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- #END# CPU Usage -->
 
             <div class="row clearfix">
                 <!-- Task Info -->
